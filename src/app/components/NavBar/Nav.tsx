6e683519a58febb913faefcd6d2c1688fa05596c
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-02-15 21:03:21 +0800

    More fixes

--- src/app/components/NavBar/Nav.tsx
+++ src/app/components/NavBar/Nav.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import styled from 'styled-components/macro';
-import { ReactComponent as DocumentationIcon } from './assets/documentation-icon.svg';
-import { ReactComponent as GithubIcon } from './assets/github-icon.svg';
-import { WrapperPage, WrapperNav } from '../Wrappers';
+import { WrapperNav } from '../Wrappers';
 
 const Nav: React.FC = () => {
   return (
