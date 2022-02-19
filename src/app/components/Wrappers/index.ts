2c27840d8bfe6b1add389fc0eb0fe2877f3a5c3b
Salvador Amba Jr <salvadorjr.amba@gmail.com>
2022-02-19 20:30:13 +0800

    New component and FX update

--- src/app/components/Wrappers/index.ts
+++ src/app/components/Wrappers/index.ts
@@ -1,13 +1,5 @@
 import styled from 'styled-components/macro';
 
-export const Flex = styled.div`
-  display: flex;
-`;
-
-export const Grid = styled.div`
-  display: grid;
-`;
-
 export const WrapperPage = styled.div`
   width: 100%;
   margin: 0 auto;
@@ -24,8 +16,20 @@ export const WrapperContent = styled.div`
 
 export const WrapperNav = styled.div`
   z-index: 100;
-  padding: 2rem;
-  width: 15%;
+  padding: 0.3rem;
   height: 100vh;
   background-color: white;
+  min-width: 8rem;
+  display: grid;
+`;
+
+export const WrapperTxtScroller = styled.div`
+  z-index: 100;
+  padding: 0.3rem;
+  height: 100vh;
+  background-color: black;
+  min-width: 2rem;
+  max-width: 2rem;
+  display: flex;
+  letter-spacing: 3px;
 `;
